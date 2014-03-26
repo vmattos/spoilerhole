b86e1bf9e6331bdb9cec876a2ec72829b4df91d4
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-03-26 19:44:24 -0300

    Model for Media and finishing MVC core structure

---
+++ app/models/media.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var mediaSchema = new Schema({
+	title: String
+});
+
+module.exports = exports = mongoose.model('Media', mediaSchema);
