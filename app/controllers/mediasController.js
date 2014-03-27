02f1311cef885dd532cf26db3d862238a4faef30
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-03-27 09:46:40 -0300

    Adding medias feature

--- app/controllers/mediasController.js
+++ app/controllers/mediasController.js
@@ -1,11 +1,31 @@
 'use strict';
 
+var Media = require('../models/media');
 var medias = {};
 
 module.exports = exports = medias;
 
 medias.index = function(req, res) {
-	res.render('index', {
-		title: 'Spoilerhole'
+	Media.find({}, function(error, medias) {
+		res.render('index', {
+			title: 'Spoilerhole',
+			medias: medias
+		});
+	});
+};
+
+medias.new = function(req, res) {
+	res.render('medias/new', {
+		title: 'New media'
+	});
+};
+
+medias.create = function(req, res, next) {
+	var media = new Media(req.body.media);
+
+	media.save(function(error, media) {
+		if(error) next(error);
+
+		res.send(media);
 	});
 };
--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,8 @@
 module.exports = function(app) {
 	var medias = require('../app/controllers/mediasController');
 
-
+	// medias routes
 	app.get('/', medias.index);
+	app.get('/media', medias.new); 
+	app.post('/media', medias.create);
 };
