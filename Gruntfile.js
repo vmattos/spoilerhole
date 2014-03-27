b95189aaeacc72db5bea6c85c120e4f4d7090650
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-03-27 09:06:41 -0300

    Adding gruntfile and jshint

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function(grunt) {
+	require('time-grunt')(grunt);
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		develop: {
+			server: {
+				file: 'server.js'
+			}
+		},
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc',
+				ignores: [ 'node_modules/**' ]
+			},
+			uses_defaults: [ 'server.js', 'app/**/*.js', 'config/**/*.js' ],
+		},
+		/*jscs: {
+			uses_defaults: '<%= jshint.uses_defaults %>'
+		},*/
+		watch: {
+			options: {
+				nospawn: true,
+				livereload: 35729
+			},
+			server: {
+				files: '<%= jshint.uses_defaults %>',
+				tasks: [ 'develop:server' ]
+			}
+		}
+	});
+
+	grunt.config.requires('watch.server.files');
+	grunt.registerTask('default', [ 'develop', 'watch' ] );
+	grunt.registerTask('test', [ 'jshint']);//, 'jscs' ] );
+};
