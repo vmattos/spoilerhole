cb864604065c43dfabfd6b4b5d667916392ea5b8
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-03-26 21:30:52 -0300

    Starting unit testing

---
+++ test/media/models.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var test = require('nodeunit');
+var Media = require('../../app/models/media');
+
+var db;
+
+module.exports = {
+    setUp: function(callback) {
+        db = mongoose.connect('mongodb://localhost/test');
+        callback();
+    },
+
+    tearDown: function(callback) {
+		Media.remove({}, function(){});
+        db.disconnect();
+        callback();        
+    },
+
+    "Should insert media with a title": function(test) {
+	
+		var media = new Media({ title: 'Lord of The Rings' });
+
+		media.save(function(error, media) {
+			if(error) console.log(error);
+
+			test.equal(media.title, 'Lord of The Rings', '"Title" should be Lord of The Rings');
+			test.done();
+		});
+	}
+};
--- test/test-runner.js
+++ test/test-runner.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 var reporter = require('nodeunit').reporters.default;
+process.env.NODE_ENV = 'test';
+
 reporter.run(['test/media/models.js']);
