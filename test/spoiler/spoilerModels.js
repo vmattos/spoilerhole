fc1bf2a6958d6c95ca1489698bfbc4c2c1ff70e0
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-03-30 00:00:06 -0300

    First unit tests for spoiler model

--- test/spoiler/spoilerModels.js
+++ test/spoiler/spoilerModels.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var test = require('nodeunit');
-//var Spoiler = require('../../app/models/spoiler');
+var Spoiler = require('../../app/models/spoiler');
 
 var db;
 
@@ -9,15 +9,15 @@ db = mongoose.connect('mongodb://localhost/test');
 module.exports = {
 
 	"Should NOT create a spoiler without a media" : function(test){
-		//var spoiler = new Spoiler();
+		var spoiler = new Spoiler();
 
-		//test.throws(spoiler.save(function(error, spoiler) {
+		test.throws(spoiler.save(function(error, spoiler) {
 			test.done()
-		//}));  
+		}));  
 	},
 
 	"Cleaning up database...": function(test) {
-		Media.remove({}, function(){});	
+		Spoiler.remove({}, function(){});	
 		db.disconnect();
 		test.done();
 	}
--- test/test-runner.js
+++ test/test-runner.js
@@ -2,4 +2,7 @@
 var reporter = require('nodeunit').reporters.default;
 process.env.NODE_ENV = 'test';
 
-reporter.run(['test/media/mediaModels.js']);
+reporter.run([
+	'test/media/mediaModels.js', 
+	'test/spoiler/spoilerModels.js'
+]);
