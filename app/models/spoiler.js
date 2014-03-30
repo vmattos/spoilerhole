3d2406e361e64f1fe101cb54c9c1ca66819ca025
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-03-30 00:03:55 -0300

    Creating spoiler model

---
+++ app/models/spoiler.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+var spoilerSchema = new Schema({
+	text: { type: String, required: true },
+	media_id: { type: ObjectId, required: true }
+});
+
+module.exports = exports = mongoose.model('Spoiler', spoilerSchema);
