9bb0d141caaa6ce0bfacb94e2e0f4ec6cacc2d11
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-03-29 23:58:52 -0300

    Renaming directory to follow standards

--- test/spoiler/spoilerModels.js
+++ test/spoiler/spoilerModels.js
@@ -1,11 +1,20 @@
 var mongoose = require('mongoose');
 var test = require('nodeunit');
+//var Spoiler = require('../../app/models/spoiler');
 
 var db;
 
 db = mongoose.connect('mongodb://localhost/test');
 
 module.exports = {
+
+	"Should NOT create a spoiler without a media" : function(test){
+		//var spoiler = new Spoiler();
+
+		//test.throws(spoiler.save(function(error, spoiler) {
+			test.done()
+		//}));  
+	},
 
 	"Cleaning up database...": function(test) {
 		Media.remove({}, function(){});	
