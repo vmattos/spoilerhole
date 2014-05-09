2627d53b7d391d9dd8703a2b89e36ef189230103
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-05-09 00:08:05 -0300

    Acceptance test: User should be able to create a new media

--- tests/acceptance/userTest.js
+++ tests/acceptance/userTest.js
@@ -1,15 +1,32 @@
 var Browser = require("zombie");
 var assert = require("assert");
+var chai = require("chai");
 
 describe("User", function() {
-  before(function(done) {
-    this.browser = new Browser();
-    this.browser
-      .visit("http://localhost:3000")
-      .then(done, done);
-  });
 
-  it("should be able to create a new media", function() {
-    assert.ok(true);
-  });
+	var browser;
+
+	before(function(done) {
+  		browser = new Browser();
+    	browser
+    		.visit("http://localhost:3000")
+    		.then(done, done);
+    });
+
+	it("should be able to create a new media", function() {
+    	return browser.clickLink("New media")
+    			.then(function() {
+
+    				return browser
+    					.fill("media[title]", "Harry Potter")
+    					.pressButton("Save")
+    				
+    			})
+    			.then(function() {
+    				var html = browser.html();
+		    		chai.expect(html).to.contain('Harry Potter');
+	    		});
+    });
+
+
 });
